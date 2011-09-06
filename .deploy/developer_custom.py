ecc8c69acfb2fd299ed7108ea824e20ffa6fa5a5
Reorx <novoreorx@gmail.com>
2011-09-06 23:47:34 +0800

    set developer_custom ignore

--- .deploy/developer_custom.py
+++ .deploy/developer_custom.py
@@ -1,17 +1,16 @@
 #coding=utf8
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        #'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'nodemix_api',
+        'ENGINE': 'mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'projcube',
         #'NAME': os.path.join(ROOT, '../MEDIA/testDB.sqlite'),
-        'USER': 'root',
-        #'USER': '',
-        'PASSWORD': 'h@*ML$A?R>L[w/e',
-        #'PASSWORD': '',
-        'HOST': '',
-        #'HOST': '10.10.10.59',
-        'PORT': '',
-        #'PORT': '3306',
+        'USER': 'root',                      # Not used with sqlite3.
+        #'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': 'nodemix',                  # Not used with sqlite3.
+        #'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        #'HOST': '10.10.10.59',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        #'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
